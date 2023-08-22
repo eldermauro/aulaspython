cfe32c49b9baee5246327a2961069229ce897cd5
Mauro <shamauro.ml@gmail.com>
2023-08-21 20:09:49 -0400

    aula 10

---
+++ aula10.py
@@ -0,0 +1,58 @@
+# armazenar mais de uma informação em variaveis
+# manter a sequencia dos dados em uma variavel
+# O lower()método retorna uma string onde todos os caracteres são minúsculos.
+# Símbolos e números são ignorados.
+
+
+#junta listas
+
+cores = ['amarelo', 'verde', 'azul', 'vermelho','roxo']
+valores = [10, 20, 30, 40, 50]
+valor = [53.00, 56.00]
+
+duas_listas = zip(cores, valores, valor)
+
+print(list(duas_listas))
+
+
+# criar uma lista de frutas a partir do input fornecido po virgulas:
+
+#O split()método divide uma string em uma lista.
+# Você pode especificar o separador, o separador padrão é qualquer espaço em branco.
+
+frutas_usuario = input('digite o nome das frutas separados por virgula: ')
+
+
+frutas_lista = frutas_usuario.split(', ')
+
+print(frutas_lista)
+
+# armazenar mais de uma informação em variaveis
+# manter a sequencia dos dados em uma variavel
+# O lower()método retorna uma string onde todos os caracteres são minúsculos.
+# Símbolos e números são ignorados.
+
+
+#junta listas
+
+cores = ['amarelo', 'verde', 'azul', 'vermelho','roxo']
+valores = [10, 20, 30, 40, 50]
+valor = [53.00, 56.00]
+
+duas_listas = zip(cores, valores, valor)
+
+print(list(duas_listas))
+
+
+# criar uma lista de frutas a partir do input fornecido po virgulas:
+
+#O split()método divide uma string em uma lista.
+# Você pode especificar o separador, o separador padrão é qualquer espaço em branco.
+
+frutas_usuario = input('digite o nome das frutas separados por virgula: ')
+
+
+frutas_lista = frutas_usuario.split(', ')
+
+print(frutas_lista)
+
